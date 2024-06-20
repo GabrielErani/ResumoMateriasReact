6d41ef54eec881d32d21db7657b0279b527d6a71
Gabriel <gabrielciriloerani@gmail.com>
2024-06-20 10:29:24 -0300

    vsfd responsividade

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import { Reactlogo, Phplogo, AlgoritmosData, Mysqllogo } from '../components/Svg
 import { CardContent, CardDescription, CardReact } from '../components/CardReact';
 const Home = () => {
   return (
-    <div className='flex flex-col justify-center items-center min-h-screen p-2 2xl:max-w-screen-xl'>
+    <div className='flex flex-col justify-center items-center min-h-screen p-2 '>
       <div className='w-full max-w-4xl bg-gray-400 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-50 border border-gray-100 flex justify-center items-center flex-col p-6'>
         <h1 className='text-center text-2xl m-4'>Resumo de meus aprendizados</h1>
         <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3'>
